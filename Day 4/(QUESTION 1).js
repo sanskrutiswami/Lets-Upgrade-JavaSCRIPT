63f5ccc1b8db114638dc63dd8c78d9e621ccf8b8
Sanskruti Swami <68231214+sanskrutiswami@users.noreply.github.com>
2020-07-19 13:21:57 +0530

    Update (QUESTION 1).js

--- Day 4/(QUESTION 1).js
+++ Day 4/(QUESTION 1).js
@@ -1,5 +1,6 @@
+/*Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, "buzz" at multiples of 5 and
+"fizzbuzz" at multiples of 3 and 5.*/
 console.log("loops")
-
 let iteration= 0
 for (let iteration=0;iteration<=100;iteration++) 
 {
