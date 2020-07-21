640ba97dc182402b2163f9ba15f79581eb81625d
Sanskruti Swami <68231214+sanskrutiswami@users.noreply.github.com>
2020-07-21 23:13:24 +0530

    Update (QUESTION 3).js

--- Day 5/(QUESTION 3).js
+++ Day 5/(QUESTION 3).js
@@ -3,13 +3,17 @@ completed todos to the console.
 API Endpoint : https://jsonplaceholder.typicode.com/todos*/
 
 console.log("hello!");
-function fetchTodo(){
+function fetchTodo()
+{
     fetch("https://jsonplaceholder.typicode.com/todos").then(response=>{console.log(response)
      return response.json()})
-     .then(data=>{
+     .then(data=>
+        {
          console.log(data);
-         for(let i=0;i<data.length;i++){
-             if(data[i].completed==true){
+         for(let i=0;i<data.length;i++)
+         {
+             if(data[i].completed==true)
+             {
                  console.log(data[i]);
              }
          }
