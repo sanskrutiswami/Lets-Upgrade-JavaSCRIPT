7f4f858fe8031fefa4f0d6d7012887c1a326d39e
Sanskruti Swami <68231214+sanskrutiswami@users.noreply.github.com>
2020-07-23 15:25:23 +0530

    Update src1.js

--- Day 1/src1.js
+++ Day 1/src1.js
@@ -1,8 +1,7 @@
 alert("Welcome to this page.click me to proceed!");
 console.prompt("WELCOME TO THIS PAGE! To proceed please enter your name:");
-console.log('%cTHIS IS A JS CODE','color:green');
+console.log('%cTHIS IS A JS CODE','color:green');//%c is used to change the color of the txt.
 console.error("ERROR ALERT! ERROR ALERT!");
 console.warn("This is a warning notification!");
-console.info('%cThis is the solution to the question 1 of assignment day 1','color:purple')
+console.info('%cThis is the solution to the question 1 of assignment day 1','color:purple')//%c is used to cahnge the color of the txt.
 console.table(['1', '3','learning','is','fun','with','letsuprage']);
-console.log(text.concat("Welcome to this page "+" This is a method of string."))
