5b520424dabb5801c7467b39422c49e259102582
Sanskruti Swami <68231214+sanskrutiswami@users.noreply.github.com>
2020-07-23 19:41:29 +0530

    Add files via upload

---
+++ Day 2/(QUESTION 1).js
@@ -0,0 +1,4 @@
+/*Write a JS code which takes input from the user and logs it in the console*/
+console.log("Hello!");
+let userinput = prompt(" Welcome! To proceed please enter your UID number: ");
+console.log(userinput);
