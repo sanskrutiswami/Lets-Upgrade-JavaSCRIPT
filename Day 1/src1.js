17305005ef0af71a7c02a3326c0802ec4d94f7cd
Sanskruti Swami <68231214+sanskrutiswami@users.noreply.github.com>
2020-07-23 15:19:22 +0530

    Add files via upload

---
+++ Day 1/src1.js
@@ -0,0 +1,8 @@
+alert("Welcome to this page.click me to proceed!");
+console.prompt("WELCOME TO THIS PAGE! To proceed please enter your name:");
+console.log('%cTHIS IS A JS CODE','color:green');
+console.error("ERROR ALERT! ERROR ALERT!");
+console.warn("This is a warning notification!");
+console.info('%cThis is the solution to the question 1 of assignment day 1','color:purple')
+console.table(['1', '3','learning','is','fun','with','letsuprage']);
+console.log(text.concat("Welcome to this page "+" This is a method of string."))
